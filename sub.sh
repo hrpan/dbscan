a3825e97f8bf9380ac98cc36f0013e13982c05ee
Hsiao-Ru Pan <b0pan@pdsf9.nersc.gov>
2016-04-11 08:04:35 -0700

    Fixed some if statement

--- sub.sh
+++ sub.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 #qsub -j y -l h_vmem=4G -l projectio=1 -o /global/homes/b/b0pan/project/unified/script/dbscan/jobLog/ dbscan.sh
-if [ "${1}"=="pre" ]; then
+if [ $1 == pre ]; then
 	echo "Submitting prescan job"
-elif [ "${1}"=="db" ]; then
+elif [ $1 == db ]; then
 	echo "Submitting dbscan job"
 fi
 qsub -j y -l h_vmem=4G -l projectio=1 -o /global/homes/b/b0pan/project/unified/script/dbscan/jobLog/ scan.sh $1
