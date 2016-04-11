1b14d661a2ffd351bcbca54526a9ab1f95c3f637
Hsiao-Ru Pan <b0pan@pdsf9.nersc.gov>
2016-04-10 23:16:36 -0700

    sub.sh now takes 1 argument (pre or db)

--- sub.sh
+++ sub.sh
@@ -1,2 +1,10 @@
 #/bin/bash
-qsub -j y -l h_vmem=4G -l projectio=1 -o /global/homes/b/b0pan/project/unified/script/dbscan/jobLog/ dbscan.sh
+#qsub -j y -l h_vmem=4G -l projectio=1 -o /global/homes/b/b0pan/project/unified/script/dbscan/jobLog/ dbscan.sh
+if [ "${1}"=="pre" ]; then
+	echo "Submitting prescan job"
+	qsub -j y -l h_vmem=4G -l projectio=1 -o /global/homes/b/b0pan/project/unified/script/dbscan/jobLog/ prescan.sh
+elif [ "${1}"=="db" ]; then
+	echo "Submitting dbscan job"
+	qsub -j y -l h_vmem=4G -l projectio=1 -o /global/homes/b/b0pan/project/unified/script/dbscan/jobLog/ dbscan.sh
+fi
+
