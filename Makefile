0192758bb7b70cfc0234687a7833f81bd6b34eb7
Hsiao-Ru Pan <b0pan@pdsf10.nersc.gov>
2016-04-13 05:20:55 -0700

    Prescan is now completely removed

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 VPATH=src
-all: dbscan prescan
+all: dbscan 
 dbscan: dbscan.cc
-	g++ -O2 -o $@ $< -lEG `root-config --cflags --libs`
-prescan: prescan.cc
 	g++ -O2 -o $@ $< -lEG `root-config --cflags --libs`
 clean:
 	rm ./*~ ./*.o ./main
--- scan.sh
+++ scan.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 source ~/conf.sh
-if [ $1 == pre ]; then
-	/global/homes/b/b0pan/project/unified/script/dbscan/prescan
-elif [ $1 == db ]; then
-	/global/homes/b/b0pan/project/unified/script/dbscan/dbscan
-fi
+/global/homes/b/b0pan/project/unified/script/dbscan/dbscan
